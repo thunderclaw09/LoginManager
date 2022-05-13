50b7b97b7eb5f6168f4ce8d75126ef2eba115894
thunderclaw09 <elisalam07@gmail.com>
2022-05-13 19:21:12 -0400

    Now you can create an account, but you cannot log into an existing one yet.

--- WriteToFile.cs
+++ WriteToFile.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WriteToFile
 {
@@ -9,7 +11,11 @@ namespace WriteToFile
         public static void Main(string[] args)
         {
 
-            String Answer;
+            string Answer;
+            string NewUsername;
+            string NewPassword;
+            string NewName;
+            // string File = "Data.txt";
 
             // Console.WriteLine("Testing. Does this work?");
 
@@ -29,19 +35,65 @@ namespace WriteToFile
             if (Answer == "Y")
             {
                 Console.WriteLine("You have logged in!");
+
+
+
             }else if (Answer == "N")
             {
-                Console.WriteLine("You have created a new account!");
+                //Console.WriteLine("You have created a new account!");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Welcome to the account setup wizard!");
+
+                Console.WriteLine("Please input your new username: ");
+                NewUsername = Console.ReadLine();
+                // await File.WriteAllTextAsync("WriteText.txt", NewUsername);
+                MyAsync(NewUsername);
+                
+
+                Console.WriteLine("Please input your new password: ");
+                NewPassword = Console.ReadLine();
+                //await File.WriteAllTextAsync("WriteText.txt", NewPassword);
+                MyAsync(NewPassword);
+
+                Console.WriteLine("Please input your name: ");
+                NewName = Console.ReadLine();
+                //await File.WriteAllTextAsync("WriteText.txt", NewName);
+                MyAsync(NewName);
+                
             }
 
-
-            
+     
 
 
             Console.ReadKey();
 
             
         }
+
+
+        // public static async Task MyAsync(string ThingToAppend)
+        // {
+        //     StreamWriter file = new StreamWriter("Data.txt", append: true);
+        //     await file.WriteLineAsync(ThingToAppend);
+        //     //file.WriteLineAsync(ThingToAppend); 
+        //     file.Close();
+        // }
+
+        public static async void MyAsync(string ThingToAppend)
+        {
+            StreamWriter file = new StreamWriter("Data.txt", append: true);
+            await file.WriteLineAsync(ThingToAppend);
+            //file.WriteLineAsync(ThingToAppend); 
+            file.Close();
+        }
+
+
+
+
+
+
+
+
 
         // void StartMessage()
         // {
