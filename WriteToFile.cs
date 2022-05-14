93d7213da352e8df9fa0521dea37ed642af1bc72
thunderclaw09 <elisalam07@gmail.com>
2022-05-13 20:11:20 -0400

    Finished the basics of the login. Still haven't done all the tests yet but right now you can create accounts and log in!

--- WriteToFile.cs
+++ WriteToFile.cs
@@ -18,13 +18,18 @@ namespace WriteToFile
 
             string Username;
             string Password;
+            string Name;
 
-            string UsernameFile = "Usernames.txt";
+            string UsernamesFile = "Usernames.txt";
             string PasswordsFile = "Passwords.txt";
             string NamesFile = "Names.txt";
 
             bool HasUsername = false;
             bool HasPassword = false;
+
+            int IndexOfName = 0;
+
+
 
             //START THE MESSAGE
             Console.WriteLine("Welcome to Login Manager!");
@@ -48,27 +53,54 @@ namespace WriteToFile
                 //     Console.WriteLine(lines[i]);
                 // }
 
+                Console.WriteLine("\n");
                 Console.WriteLine("Please input your username:");
                 Username = Console.ReadLine();
                 
+                Console.WriteLine("\n");
                 Console.WriteLine("Please input your password:");
                 Password = Console.ReadLine();
 
-                string[] UsernameArray = File.ReadAllLines(UsernameFile);
+                string[] UsernameArray = File.ReadAllLines(UsernamesFile);
+                string[] PasswordArray = File.ReadAllLines(PasswordsFile);
+                string[] NameArray = File.ReadAllLines(NamesFile);
 
                 for (int i = 0; i < UsernameArray.Length; i++)
                 {
                     string line = UsernameArray[i];
                     if (Username == line)
                     {
-                        Console.WriteLine("You have a username entry!");
+                        // Console.WriteLine("\n");
+                        // Console.WriteLine("You have a username entry!");
                         HasUsername = true;
+                        IndexOfName = i;
                     }
                 }
 
                 if (HasUsername == true)
                 {
-                    Console.WriteLine("You are now logged in! (No password yet.)");
+                    // Console.WriteLine("You are now logged in! (No password yet.)");
+                    for (int i = 0; i < PasswordArray.Length; i++)
+                    {
+                        string line = PasswordArray[i];
+                        if (Password == line)
+                        {
+                            // Console.WriteLine("You have a password entry!");
+                            HasPassword = true;
+                        }
+                    }
+
+                    if (HasPassword == true)
+                    {
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You have successfully logged in!");
+                        Name = NameArray[IndexOfName];
+                        Console.WriteLine("Welcome, "+Name);
+                    }else
+                    {
+                        Console.WriteLine("You have not put in a correct password.");
+                    }
+                    
                 }else
                 {
                     Console.WriteLine("You either do not have an account yet or you didn't put in your username correctly.");
@@ -84,21 +116,25 @@ namespace WriteToFile
 
                 Console.WriteLine("Please input your new username: ");
                 NewUsername = Console.ReadLine();
-                MyAsync(NewUsername, UsernameFile);
+                MyAsync(NewUsername, UsernamesFile);
                 
-
+                Console.WriteLine("\n");
                 Console.WriteLine("Please input your new password: ");
                 NewPassword = Console.ReadLine();
                 MyAsync(NewPassword, PasswordsFile);
 
+                Console.WriteLine("\n");
                 Console.WriteLine("Please input your name: ");
                 NewName = Console.ReadLine();
                 MyAsync(NewName, NamesFile);
+
+                Console.WriteLine("\n");
+                Console.WriteLine("You have successfully created an account. Restart the console to be able to log in!");
                 
             }
 
      
-
+            Console.WriteLine("\n");
             Console.WriteLine("Press Enter to continue...");
             Console.ReadKey();
 
